55da115bcd5514b64e3782ebcd59bc2d588a1650
rahmann99 <uchiha.rahmann@gmail.com>
2026-08-27 21:32:50 +1000

    Page 236 refactoring the check

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,17 @@
 import sys
 import pygame
 
+from settings import Settings
 class AlienInavsion:
     """ to manage assets and behaviour"""
 
     def __init__(self):
         """ initialize and create resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(self.settings.screen_width,self.settings.screen_height)
         pygame.display.set_caption("Alien Huahhahahahah")
     
     def run_game(self):
@@ -17,6 +20,7 @@ class AlienInavsion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             """ make the most recent screen visible"""
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip() 
 
 if __name__ == '__main__':
